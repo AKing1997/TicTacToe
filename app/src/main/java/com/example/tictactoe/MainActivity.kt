c07647c8bd5fed3329b6ba31915c89c96fcc6d36
CfAnkit <cf21ankit.kumar@iesjoandaustria.org>
2023-05-02 15:42:20 +0200

    Actualizacion

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -3,22 +3,15 @@ package com.example.tictactoe
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var b1: Button
-    private lateinit var b2: Button
-    private lateinit var b3: Button
-    private lateinit var b4: Button
-    private lateinit var b5: Button
-    private lateinit var b6: Button
-    private lateinit var b7: Button
-    private lateinit var b8: Button
-    private lateinit var b9 : Button;
-    private lateinit var x : RadioButton;
-    private lateinit var o : RadioButton;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
     }
 }
